47245afef3ca45636e06931aa2d813c8510f17ab
SeanRosenstein <seanr152@nycstudents.net>
2025-03-03 17:03:19 +0000

    Final

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -11,9 +11,20 @@ public class Game{
             return "Player 2 wins!";
         }  
         if (Utility.getHandRanking(p1Hand)==Utility.getHandRanking(p2Hand)){
-            
+
+            int p1High = Math.max(Utility.getRankValue(p1.getHand().get(1).getRank()),Utility.getRankValue(p1.getHand().get(0).getRank()));
+            int p2High = Math.max(Utility.getRankValue(p2.getHand().get(1).getRank()),Utility.getRankValue(p2.getHand().get(0).getRank()));
+            if (p1High>p2High){
+                return "Player 1 wins!";
+            }
+            else if (p1High<p2High){
+                return "Player 2 wins!";
+            }
+            else{
+                return "Tie!";
+            }
         }  
-        return "1";
+        return "-1";
     }
 
     public static void play(){ //simulate card playing
